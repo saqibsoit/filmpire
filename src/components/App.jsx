88252d3b785e4b550ef97a699af2594275fcf68e
saqibsoit <saqibsoit@hotmail.com>
2023-10-22 21:37:35 +0500

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
+
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>App</div>
+
+    <div className={classes.root}>
+      <NavBar />
+
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
+        <Routes>
+          <Route exact path='/movie/:id' element={<MovieInformation />} />
+          <Route exact path='/actors/:id' element={<Actors />} />
+          <Route exact path='/' element={<Movies />} />
+          <Route exact path='/profile/:id' element={<Profile />} />
+
+        </Routes>
+      </main>
+    </div>
   )
 }
 
